d55c9e5cc4e56f7280e104c83265ad4e546e621b
Sireth <mltya.usov04@gmail.com>
2024-06-05 22:24:11 +0300

    Project: connection of all necessary libraries

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file main.cpp
+ *
+ * @date 05.06.2024
+ * @author d.usov\@kpda.ru
+ *
+ * (c) 2024, SWD Embedded Systems Limited, http://www.kpda.ru
+ */
+
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
